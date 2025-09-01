915452539dec5af2ef1d6c871fec275ae11da461
Attreya <attreya01@gmail.com>
2025-09-01 16:07:05 +0530

    sending data from 1 page to another

--- lib/views/pages/settings_page.dart
+++ lib/views/pages/settings_page.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class SettingsPage extends StatefulWidget {
-  const SettingsPage({super.key});
+  final String title;
+  const SettingsPage({super.key, required this.title});
 
   @override
   State<SettingsPage> createState() => _SettingsPageState();
@@ -16,14 +17,7 @@ class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Settings'),
-        leading: BackButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
+      appBar: AppBar(title: Text(widget.title)),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
