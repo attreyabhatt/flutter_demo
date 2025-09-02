5a43bf88ff1d67b98cc8b156b35977ffd4504181
Attreya <attreya01@gmail.com>
2025-09-03 05:19:54 +0530

    dispose and init

--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_demo/views/widgets/hero_widget.dart';
 
-TextEditingController controller = TextEditingController();
-
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -11,6 +9,23 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  TextEditingController controller = TextEditingController();
+
+  //triggers when the login page is opened
+  @override
+  void initState() {
+    print('initState');
+    super.initState();
+  }
+
+  //trigger when the login page is closeed.
+  @override
+  void dispose() {
+    //removing the controller after the login page is closed
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
