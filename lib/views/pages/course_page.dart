d0429da2c777285dfb6a946da989956906e32ae5
Attreya <attreya01@gmail.com>
2025-09-05 15:36:43 +0530

    api stuff

--- lib/views/pages/course_page.dart
+++ lib/views/pages/course_page.dart
@@ -1,18 +1,67 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_demo/views/widgets/hero_widget.dart';
+import 'dart:convert' as convert;
 
-class CoursePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_demo/data/classes/activity_class.dart';
+import 'package:flutter_demo/views/widgets/hero_widget.dart';
+import 'package:http/http.dart' as http;
+
+class CoursePage extends StatefulWidget {
   const CoursePage({super.key});
+
+  @override
+  State<CoursePage> createState() => _CoursePageState();
+}
+
+class _CoursePageState extends State<CoursePage> {
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
+  Future getData() async {
+    var url = Uri.https('bored-api.appbrewery.com', '/random');
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      return Activity.fromJson(
+        convert.jsonDecode(response.body) as Map<String, dynamic>,
+      );
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: SingleChildScrollView(
-          child: Column(children: [HeroWidget(title: 'Course')]),
-        ),
+      body: FutureBuilder(
+        future: getData(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasData) {
+            Activity activity = snapshot.data;
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    HeroWidget(title: 'Course'),
+                    Text(activity.activity),
+                  ],
+                ),
+              ),
+            );
+          } else {
+            return Center(child: Text('Error'));
+          }
+        },
       ),
     );
   }
